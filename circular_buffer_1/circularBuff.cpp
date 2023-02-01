df60b891ec8c880766a6d5086333b0fcef860499
Peace-duck <79522220302@yandex.ru>
2023-02-01 03:57:16 +0300

    норм работает push back

--- circular_buffer_1/circularBuff.cpp
+++ circular_buffer_1/circularBuff.cpp
@@ -4,45 +4,61 @@ template <class Type>
 class circularBuff {
 private:
 	Type* buff;
-	size_t length = 0;
-	size_t indexInsert = 0;
-	size_t offsetPoint = 0;
+	size_t length;
+	size_t indexInsert = 0;			
+	size_t offsetPoint = 0;			
+	bool flagReplace;				//флаг перезаписи
 public:
-	explicit circularBuff(size_t length) :length(length), buff(new Type[length]) {}
+	explicit circularBuff(size_t length = 0) :length(length), 
+		 buff(new Type[length]), flagReplace(!length) {}
+	~circularBuff() { this->clear();}
 	Type& operator[] (const size_t index);
 	size_t size() { return length; }
 	void push_back(const Type& value);
+	void push_front(const Type& value);
+	void clear();
 };
 
-
 template<class Type>
-Type& circularBuff<Type>::operator[]( size_t index)
+Type& circularBuff<Type>::operator[](const size_t index)
 {
-	index += offsetPoint;
-	if (index > length) 
+	size_t replaceIndex = index + offsetPoint;		
+	if (replaceIndex == length)
 	{
-		index -= length;
+		replaceIndex -= length;
 	}
-	return buff[index];
+	return buff[replaceIndex];
 }
 
 template<class Type>
 void circularBuff<Type>::push_back(const Type& value)
 {
-	if (indexInsert < length)
+	// [1] [2] [3] [4] -> [5][2][3][4]
+	if (indexInsert == length)  
 	{
-		indexInsert++;
+		indexInsert = 0;
+		flagReplace = true;
 	}
-	else
+	if (flagReplace)
 	{
 		offsetPoint++;
-		indexInsert = 0;
 		if (offsetPoint == length)
 		{
 			offsetPoint = 0;
 		}
-	}
+	}	
 	buff[indexInsert] = value;
+	indexInsert++;
+}
+
+template<class Type>
+void circularBuff<Type>::clear()
+{
+	if (buff != NULL)
+	{
+		delete[] buff;
+		buff = nullptr;
+	}
 }
 
 
@@ -55,8 +71,9 @@ int main() {
 	buffer.push_back(4);
 	buffer.push_back(5);
 	buffer.push_back(6);
-	//buffer.push_back(7);
-	//buffer.push_back(8);
+	buffer.push_back(7);
+	buffer.push_back(8);
+	//buffer.push_back(9);
 	for (size_t index = 0; index < buffer.size(); index++) {
 		std::cout<< buffer[index]<< std::endl;
 	}
