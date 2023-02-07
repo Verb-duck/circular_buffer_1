d1b551cee873be24be2c74ae24759e92018a5463
Peace-duck <79522220302@yandex.ru>
2023-02-07 16:01:28 +0300

    Подправил pop back и pop front

--- circular_buffer_1/circularBuff.cpp
+++ circular_buffer_1/circularBuff.cpp
@@ -2,7 +2,7 @@
 
 #define DEBUGGING 1
 #if (DEBUGGING == 1)
-#define PRINT_BUFF for (size_t index = 0; index < length; index++)\
+#define PRINT_BUFF for (size_t index = 0; index < capacitty; index++)\
 	std::cout << buff[index] << " "; \
 	std::cout << std::endl;
 #else PRINT_BUFF
@@ -15,28 +15,28 @@ template <class Type>
 class circularBuff {
 private:
 	Type* buff;
-	size_t length;
+	size_t capacitty;
 	size_t count = 0;
 	Type* head;
 	Type* tail;
 	bool flagReplace = false;		 //ôëàã ïåðåçàïèñè
 public:
-	explicit circularBuff(size_t length = 0);
+	explicit circularBuff(size_t capacitty);
 	~circularBuff() { this->clear(); }
 	size_t size() { return count; }
 	void push_back(const Type& value);
 	void push_front(const Type& value);
-	bool pop_back(Type& out_value);
-	bool pop_front(Type& out_value);
+	bool pop_back(Type& outValue);
+	bool pop_front(Type& outValue);
 	void clear();
 };
 
 template<class Type>
-circularBuff<Type>::circularBuff(size_t length)
-	:length(length), buff(new Type[length]) {
-	flagReplace = !length;
+circularBuff<Type>::circularBuff(size_t capacitty)
+	:capacitty(capacitty), buff(new Type[capacitty]) {
+	flagReplace = !capacitty;
 	head = buff;
-	tail = &buff[length - 1];
+	tail = &buff[capacitty - 1];
 }
 
 template<class Type>
@@ -44,16 +44,12 @@ void circularBuff<Type>::push_back(const Type & value)
 {
 	// [1][2][3][4] -> [5][2][3][4]  -> [5][6][3][4]
 	//  ^head               ^head               ^head
-	if (!flagReplace)				//åñëè íå çàïîëíåí
-	{
-		if (++count == length)
-			flagReplace = true;
-	}
-	else
+	if (count == capacitty)	//åñëè çàïîëíåí
 		tail = head;
+	else
+		++count;
 	*head = value;
-	++head;
-	if (head == &buff[length])
+	if (++head == &buff[capacitty])
 		head = buff;
 	PRINT_BUFF
 }
@@ -62,37 +58,32 @@ template<class Type>
 void circularBuff<Type>::push_front(const Type & value)
 {
 	// [1][2][3][4] -> [1][2][3][5] -> [1][2][6][5]
-	//             ^tail          ^tail        ^tail
-	if (!flagReplace)				//åñëè íå çàïîëíåí
-	{
-		if (++count == length)
-			flagReplace = true;
-	}
-	else
+	//           ^tail        ^tail        ^tail
+	if (count == capacitty)
 		head = tail;
+	else
+		++count;
 	*tail = value;
 	if (tail == buff)
-		tail = &buff[length - 1];
+		tail = &buff[capacitty - 1];
 	else
 		--tail;
 	PRINT_BUFF
 }
 
 template<class Type>
-bool circularBuff<Type>::pop_back(Type & out_value)
+bool circularBuff<Type>::pop_back(Type& outValue)
 {
 	// [5][2][3][4] -> [x][2][3][4] -> [x][2][3][x]
-	//       ^head        ^head                     ^head
+	//     ^head        ^head                    ^head
 	if (count)					//åñëè åñòü ÷òî áðàòü
 	{
 		--count;
 		if (head == buff)
-			head = &buff[length - 1];
+			head = &buff[capacitty - 1];
 		else
 			--head;
-		out_value = *head;
-		//remove
-		*head = 0;
+		outValue = *head;
 		PRINT_BUFF
 			return true;
 	}
@@ -102,18 +93,16 @@ bool circularBuff<Type>::pop_back(Type & out_value)
 }
 
 template<class Type>
-bool circularBuff<Type>::pop_front(Type & out_value)
+bool circularBuff<Type>::pop_front(Type& outValue)
 {
 	// [5][2][3][4] -> [5][x][3][4] -> [5][x][x][4]
-	//  ^tail               ^tail              ^tail
+	//  ^tail              ^tail              ^tail
 	if (count)					//åñëè åñòü ÷òî áðàòü
 	{
 		--count;
-		if (++tail == &buff[length])
+		if (++tail == &buff[capacitty])
 			tail = buff;
-		out_value = *tail;
-		//remove
-		*tail = 0;
+		outValue = *tail;
 		PRINT_BUFF
 			return true;
 	}
@@ -134,6 +123,7 @@ void circularBuff<Type>::clear()
 #endif
 
 int main() {
+	int t;
 	circularBuff<int> buffer(4);
 	buffer.push_back(1);
 	buffer.push_back(2);
@@ -150,16 +140,13 @@ int main() {
 	//buffer.push_front(2);
 	//buffer.push_front(3);
 	//buffer.push_back(8);
-	int t;
-	t = 3;
-	//buffer.pop_back(t);
-	//buffer.pop_back(t);
-	//buffer.pop_back(t);
-	//buffer.pop_back(t);
-	//buffer.push_back(3);
+	buffer.pop_back(t);
+	//buffer.pop_back();
+	//buffer.pop_back();
+	buffer.push_back(5);
 	buffer.pop_front(t);
-	buffer.pop_front(t);
-	buffer.pop_front(t);
+	//buffer.pop_front();
+	//buffer.pop_front();
 
 	return 0;
 }
